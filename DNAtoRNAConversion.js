95af78035ac39cda9a0a62e6223bf182dc6f96ed
gabrielpadma <yggderasil@gmail.com>
2024-02-08 11:44:44 +0700

    6/366

---
+++ DNAtoRNAConversion.js
@@ -0,0 +1,12 @@
+//cara 1
+// const DNAtoRNA = (dna) => dna.toUpperCase().replaceAll("T", "U");
+
+// cara2
+// const DNAtoRNA = (dna) => {
+//   return [...dna]
+//     .map((dna) => {
+//       if (dna == "T") return "U";
+//       return dna;
+//     })
+//     .join("");
+// };
